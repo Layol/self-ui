b5a0356711711fcab596ab9ecba165139bfe3ef3
llliao <1656641402@qq.com>
2022-03-04 17:29:58 +0800

    tasklist

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>
-
+  <div class="list-items">
+    <template v-if="loading">
+      loading
+    </template>
+    <template v-else-if="isEmpty">
+      empty
+    </template>
+    <template v-else>
+      <Task 
+        v-for="task in taks"
+        :key="task.id"
+        :task="task"
+        @archive-task="onArchiveTask"
+        @pin-task="onPinTask"
+      />
+    </template>
   </div>
 </template>
 
 <script>
+import Task from './Task.vue'
   export default {
     name: 'task-list'
   }
